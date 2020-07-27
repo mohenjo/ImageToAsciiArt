822cb5d2f0bf5383bc4097a7a44c2edc9ecf2b43
Haennim Park <66149782+mohenjo@users.noreply.github.com>
2020-07-27 21:11:41 +0900

    Add solution

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using ImageToAsciiArt;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Bitmap sampleBitmap = Properties.Resources.testimg;
+            /*
+            string OriginalImagePath = Path.Combine(System.Environment.CurrentDirectory, "testimg.jpg");
+            sampleBitmap.Save(OriginalImagePath);
+            System.Diagnostics.Process.Start(OriginalImagePath);
+            */
+
+            AsciiArt asciiArt = new AsciiArt(sampleBitmap);
+
+        }
+    }
+}
---
+++ ImageToAsciiArt/AsciiArt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageToAsciiArt
+{
+    /// <summary>
+    /// 아스키 아트의 표현을 위한 클래스
+    /// </summary>
+    public class AsciiArt
+    {
+        /// <summary>
+        /// AsciiArt 클래스의 인스턴스를 생성합니다.
+        /// </summary>
+        /// <param name="bitmap">원본 이미지의 비트맵</param>
+        public AsciiArt(Bitmap bitmap)
+        {
+            this.OriginalBitmap = bitmap;
+        }
+
+        /// <summary>
+        /// 원본 이미지의 비트맵
+        /// </summary>
+        public Bitmap OriginalBitmap { get; }
+    }
+}
