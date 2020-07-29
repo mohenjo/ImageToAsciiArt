80bbe3863b9535c86fb4b64eeace4fa73dab491a
Haennim Park <66149782+mohenjo@users.noreply.github.com>
2020-07-29 14:59:43 +0900

    Update test console project

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using ImageToAsciiArt;
 using System.Drawing;
-using ImageToAsciiArt;
+using System.IO;
+using System.Text;
 
 namespace ConsoleTest
 {
@@ -13,6 +9,7 @@ namespace ConsoleTest
     {
         static void Main()
         {
+            // 원본 이미지
             Bitmap sampleBitmap = Properties.Resources.sample_image;
             string OriginalImagePath = Path.Combine(System.Environment.CurrentDirectory, "sample_image.jpg");
             sampleBitmap.Save(OriginalImagePath);
@@ -20,15 +17,17 @@ namespace ConsoleTest
 
             AsciiArt asciiArt = new AsciiArt(sampleBitmap);
 
-            //string asciiArtString = asciiArt.GetAsciiArt(200, true);
-            //string AsciiArtPath = Path.Combine(System.Environment.CurrentDirectory, "sample_image.txt");
-            //File.WriteAllText(AsciiArtPath, asciiArtString, Encoding.Default);
-            //System.Diagnostics.Process.Start(AsciiArtPath);
+            // 아스키 아트 생성 - text
+            string asciiArtText = asciiArt.GetAsciiArt(200, AsciiArt.ArtType.Text);
+            string AsciiArtTextPath = Path.Combine(System.Environment.CurrentDirectory, "sample_image.txt");
+            File.WriteAllText(AsciiArtTextPath, asciiArtText, Encoding.Default);
+            System.Diagnostics.Process.Start(AsciiArtTextPath);
 
-            string asciiArtString = asciiArt.GetAsciiArtHtml(200, AsciiArt.ArtType.Html, true);
-            string AsciiArtPath = Path.Combine(System.Environment.CurrentDirectory, "sample_image.html");
-            File.WriteAllText(AsciiArtPath, asciiArtString, Encoding.Default);
-            System.Diagnostics.Process.Start(AsciiArtPath);
+            // 아스키 아트 생성 - html
+            string asciiArtHtml = asciiArt.GetAsciiArt(200, AsciiArt.ArtType.Html);
+            string AsciiArtHtmlPath = Path.Combine(System.Environment.CurrentDirectory, "sample_image.html");
+            File.WriteAllText(AsciiArtHtmlPath, asciiArtHtml, Encoding.Default);
+            System.Diagnostics.Process.Start(AsciiArtHtmlPath);
         }
     }
 }
